af7a26ff9177aacbef02bb14116250ba4e4c6a3d
teste <teste@teste.com>
2017-02-24 10:23:51 -0300

    ViewModels

---
+++ Contoso.Shop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Contoso.Shop.Model.Catalog.Services;
+using Contoso.Shop.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Contoso.Shop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await productService.GetAll();
+
+            var vm = new ListProductsViewModel
+            {
+                Products = products
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ Contoso.Shop.Web/ViewModels/ListProductsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Contoso.Shop.Model.Catalog;
+
+namespace Contoso.Shop.Web.ViewModels
+{
+    public class ListProductsViewModel
+    {
+        public ListProductsViewModel()
+        {
+            // Array.Empty<Product>
+            Products = new List<Product>();
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
