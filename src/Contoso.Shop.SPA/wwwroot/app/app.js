6369da02d63cdfffab07f42b75adef7155832d12
teste <teste@teste.com>
2017-02-24 16:29:36 -0300

    spa

---
+++ src/Contoso.Shop.SPA/wwwroot/app/app.js
@@ -0,0 +1,32 @@
+﻿// definindo módulo raiz
+angular.module('app', []);
+
+angular.module('app').
+    controller('createProductController', createProductController);
+
+var apiBaseAddress = 'http://localhost:51309';
+
+function createProductController($http)
+{
+    var vm = this;
+
+    vm.form = {};
+    vm.save = save;
+
+    function save()
+    {
+        // TODO: Criar combo de seleção
+        vm.form.departamentId = 1;
+
+        $http.post(apiBaseAddress + '/products', vm.form).
+            then(showSuccess);
+
+        console.log(vm.form);
+    }
+
+    function showSuccess()
+    {
+        vm.form = {};
+        window.alert('Sucesso');
+    }
+}
